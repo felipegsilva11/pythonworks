ea83f63dc00fe5488eb43205e4cebf0455228cec
felipegsilva11 <felipe1997silva11@hotmail.com>
2024-09-10 19:48:00 -0300

    valores maior e menor ou iguais

---
+++ desafiospython/desafio038.py
@@ -0,0 +1,8 @@
+num1 = int(input('Primeiro numero: '))
+num2 = int(input('Segundo  numero: '))
+if num1 > num2:
+    print('O Primeiro valor é maior.')
+elif num2 > num1:
+    print('O segundo valor é maior.')
+else: 
+    print('Os dois valores são IGUAIS.')
